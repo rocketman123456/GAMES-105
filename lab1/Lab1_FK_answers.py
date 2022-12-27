5931ef517dfac07b740ab16b858e4a0c662b8c17
rocketman123456 <759094438@qq.com>
2022-12-27 23:26:36 +0800

    add to function for homework

--- lab1/Lab1_FK_answers.py
+++ lab1/Lab1_FK_answers.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
+
 def load_motion_data(bvh_file_path):
     """part2 辅助函数，读取bvh文件"""
     with open(bvh_file_path, 'r') as f:
@@ -18,7 +19,6 @@ def load_motion_data(bvh_file_path):
     return motion_data
 
 
-
 def part1_calculate_T_pose(bvh_file_path):
     """请填写以下内容
     输入： bvh 文件路径
@@ -30,9 +30,43 @@ def part1_calculate_T_pose(bvh_file_path):
     Tips:
         joint_name顺序应该和bvh一致
     """
-    joint_name = None
-    joint_parent = None
-    joint_offset = None
+    joint_name = []
+    joint_parent = []
+    joint_stack_list = []
+    joint_offset = []
+    my_joint_dict = {}
+
+    with open(bvh_file_path, 'r') as f:
+        lines = f.readlines()
+        for i in range(len(lines)):
+            line = [name for name in lines[i].split()]
+            if line[0] == "HIERARCHY":
+                continue
+            if line[0] == "MOTION":
+                break
+            if line[0] == "ROOT" or line[0] == "JOINT":
+                joint_name.append(line[-1])
+                joint_stack_list.append(line[-1])
+            if line[0] == "End":
+                joint_name.append(joint_name[-1]+"_end")
+                joint_stack_list.append(joint_name[-1])
+            if line[0] == "OFFSET":
+                joint_offset.append([float(line[1]), float(line[2]), float(line[3])])
+            if line[0] == "}":
+                joint_index = joint_stack_list.pop()
+                if joint_stack_list == []:
+                    continue
+                else:
+                    my_joint_dict[joint_index] = joint_stack_list[-1]
+    for i in joint_name:
+        if i == "RootJoint":
+            joint_parent.append(-1)
+        else:
+            joint_parent_name = my_joint_dict[i]
+            joint_parent.append(joint_name.index(joint_parent_name))
+    joint_offset = np.array(joint_offset).reshape(-1, 3)
+    # print(joint_name)
+    # print(joint_parent)
     return joint_name, joint_parent, joint_offset
 
 
@@ -48,8 +82,38 @@ def part2_forward_kinematics(joint_name, joint_parent, joint_offset, motion_data
         1. joint_orientations的四元数顺序为(x, y, z, w)
         2. from_euler时注意使用大写的XYZ
     """
-    joint_positions = None
-    joint_orientations = None
+    joint_positions = []
+    joint_orientations = []
+
+    frame_data = motion_data[frame_id]
+    frame_data = frame_data.reshape(-1, 3)
+
+    # calculate all quaternion
+    quaternion = R.from_euler('XYZ', frame_data[1:], degrees=True).as_quat()
+
+    # end joint don't have motion data, we will create one here
+    end_index = []
+    for i in joint_name:
+        if "_end" in i:
+            end_index.append(joint_name.index(i))
+    for i in end_index:
+        quaternion = np.insert(quaternion, i, [0,0,0,1], axis=0)
+
+    for index, parent in enumerate(joint_parent):
+        if parent==-1:
+            joint_positions.append(frame_data[0])
+            joint_orientations.append(quaternion[0])
+        else:
+            quat = R.from_quat(quaternion)
+            rotation = R.as_quat(quat[index] * quat[parent])
+            joint_orientations.append(rotation)
+            joint_orientations_quat = R.from_quat(joint_orientations)
+
+            offset_rotation = joint_orientations_quat[parent].apply(joint_offset[index])
+            joint_positions.append(joint_positions[parent] + offset_rotation)
+
+    joint_positions = np.array(joint_positions)
+    joint_orientations = np.array(joint_orientations)
     return joint_positions, joint_orientations
 
 
